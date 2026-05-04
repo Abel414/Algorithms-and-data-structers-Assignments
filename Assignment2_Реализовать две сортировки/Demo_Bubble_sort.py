56f6e3ebebdd84536774e9131bb96a41aa2c9645
Abel414 <mhoneabel70@gmail.com>
2026-05-04 23:26:43 +0300

    Create Demo_Bubble_sort.py

---
+++ Assignment2_Реализовать две сортировки/Demo_Bubble_sort.py
@@ -0,0 +1,11 @@
+from sorts import bubble_sort, merge_sort
+
+def main():
+    original = [64, 34, 25, 12, 22, 11, 90]
+    print("Original list:", original)
+  
+    data1 = original.copy()
+    bubble_sort(data1)
+    print("\nBubble sort (O(n²)):", data1)
+if __name__ == "__main__":
+    main()
