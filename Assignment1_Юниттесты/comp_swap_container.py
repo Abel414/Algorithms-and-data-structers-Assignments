6849eacc622db1137f32d7ab7cac8c425edd09e7
Abel414 <mhoneabel70@gmail.com>
2026-06-09 02:22:27 +0300

    code update

--- Assignment1_Юниттесты/comp_swap_container.py
+++ Assignment1_Юниттесты/comp_swap_container.py
@@ -1,4 +1,6 @@
+
 class CompSwapList:
+    
     def __init__(self, data):
         self._data = list(data)
         self._comps = 0
@@ -12,20 +14,9 @@ class CompSwapList:
         self._swaps += 1
         self._data[i], self._data[j] = self._data[j], self._data[i]
     
-    def __len__(self):
-        return len(self._data)
-    
-    def __getitem__(self, idx):
-        return self._data[idx]
-    
-    def __setitem__(self, idx, value):
-        self._data[idx] = value
-    
-    def __iter__(self):
-        return iter(self._data)
-    
-    def __repr__(self):
-        return repr(self._data)
+    def reset_stats(self):
+        self._comps = 0
+        self._swaps = 0
     
     @property
     def comps(self):
@@ -35,6 +26,17 @@ class CompSwapList:
     def swaps(self):
         return self._swaps
     
-    def reset_stats(self):
-        self._comps = 0
-        self._swaps = 0
+    def __len__(self):
+        return len(self._data)
+    
+    def __getitem__(self, index):
+        return self._data[index]
+    
+    def __setitem__(self, index, value):
+        self._data[index] = value
+    
+    def __iter__(self):
+        return iter(self._data)
+    
+    def __repr__(self):
+        return repr(self._data)
